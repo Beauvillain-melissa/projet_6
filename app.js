3ed96ce9ddc93d546b92d02f3ec74a2babbd9781
Melissa <beauvillainmelissa27@gmail.com>
2022-01-18 22:19:55 +0100

    correction route + correction err CORS

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const path = require ('path');
+const path = require('path');
 
 const Utilisateur = require('./models/utilisateur');
 const Sauce = require('./models/sauce');
-
-const stuffRoutes = require('./routes/stuff');
-const userRoutes = require('./routes/utilisateurs');
-
-
 
 const multer = require('./middleware/multer-config');
 
@@ -18,7 +13,7 @@ mongoose.connect('mongodb+srv://melissa:beauvillain@cluster0.gq4y2.mongodb.net/m
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  
+
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
@@ -33,14 +28,17 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/utilisateurs');
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use ('/api/stuff',stuffRoutes);
-app.use ('/api/auth',userRoutes);
+app.use('/api', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 
 //POST+verb cree et enregistre nvl sauce 
-app.post('/api/sauces', multer, (req, res, next) => { 
-  const sauceObject = JSON.parse(req.body.sauce);  
+app.post('/api/sauces', multer, (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
 
   const sauce = new Sauce({
     name: sauceObject.name,
@@ -49,21 +47,21 @@ app.post('/api/sauces', multer, (req, res, next) => {
     mainPepper: sauceObject.mainPepper,
     imageUrl: `${path.join(__dirname, 'images')}/${req.file.filename}`,
     heat: sauceObject.heat,
-    likes:0,
-    dislikes:0,
-    usersLiked:[],
-    usersDisliked:[],
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: [],
   });
 
   thing.save()
     .then(() => res.status(201).json({ message: 'Utilisateur enregistré !' }))
     .catch(error => res.status(400).json({ error }));
 });
-  //delete req.body._id;
-  //const thing = new Utilisateur({
-   // ...req.body
-  //});
-  
+//delete req.body._id;
+//const thing = new Utilisateur({
+// ...req.body
+//});
+
 //});
 
 
@@ -73,13 +71,13 @@ app.post('/api/sauces', multer, (req, res, next) => {
 //PUT mettre a jour avec id
 app.put('/api/sauces/:id', (req, res, next) => {
   sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
+    .then(() => res.status(200).json({ message: 'Sauce modifié !' }))
     .catch(error => res.status(400).json({ error }));
 });
 //DELETE supprime avec id
 app.delete('/api/sauces/:id', (req, res, next) => {
   sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
+    .then(() => res.status(200).json({ message: 'Sauce supprimé !' }))
     .catch(error => res.status(400).json({ error }));
 });
 
